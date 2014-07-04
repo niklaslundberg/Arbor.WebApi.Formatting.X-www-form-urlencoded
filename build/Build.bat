509fa94e2507eaacb7f9ccb36a736bb588d1e031
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-07-04 18:03:02 +0200

    Added Build.bat.

---
+++ build/Build.bat
@@ -0,0 +1,8 @@
+SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.X.Vcs.Branch.Name=develop
+SET Version.Build=0
+SET Version.Major=0
+SET Version.Minor=1
+SET Version.Patch=3
+
+CALL Build.exe
