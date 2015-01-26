26e7110722fd1d071c1ec59a0b99b905cf7a337c
Niklas Lundberg <niklas.lundberg@jayway.com>
2015-01-26 10:49:30 +0100

    Increased version number to 1.0.0.

--- build/Build.bat
+++ build/Build.bat
@@ -1,8 +1,8 @@
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
-SET Arbor.X.Vcs.Branch.Name=develop
-SET Version.Build=0
-SET Version.Major=0
-SET Version.Minor=1
-SET Version.Patch=3
-
-CALL Build.exe
+SET Arbor.X.Vcs.Branch.Name=release-1.0.0
+SET Version.Major=1
+SET Version.Minor=0
+SET Version.Patch=0
+SET Version.Build=1
+SET Arbor.X.NuGet.Package.Artifacts.KeepBinaryAndSymbolTogetherEnabled=false
+CALL "%~DP0Build.exe"
