4cc4397fd92b36d1706c9580aced6ddc2ffaabbe
Niklas Lundberg <info@nlundberg.se>
2017-05-03 13:25:37 +0200

    Started support for complex types.

---
+++ src/Arbor.WebApi.Formatting.HtmlForms.Tests.Unit/ComplexTypes/ItemWithServices.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Arbor.WebApi.Formatting.HtmlForms.Tests.Unit.ComplexTypes
+{
+    public class ItemWithServices
+    {
+        public override string ToString()
+        {
+            string services = Services != null
+                ? string.Join(", ", Services.Select(service => service.ToString()))
+                : "No Services";
+
+            return $"{nameof(Description)}: {Description}, {nameof(Services)}: {services}";
+        }
+
+        public string Description { get; set; }
+
+        public int NumberOfItems { get; set; }
+
+        public List<Service> Services { get; set; }// = new List<Service>();
+    }
+}
--- src/Arbor.WebApi.Formatting.HtmlForms/FormsExtensions.cs
+++ src/Arbor.WebApi.Formatting.HtmlForms/FormsExtensions.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Net.Http.Formatting;
+using System.Reflection;
 using Newtonsoft.Json;
 
 namespace Arbor.WebApi.Formatting.HtmlForms
@@ -11,44 +13,70 @@ namespace Arbor.WebApi.Formatting.HtmlForms
     {
         public static object ParseFromCollection<T>(this FormDataCollection formDataCollection)
         {
-            return ParseFromCollection(formDataCollection, typeof (T));
+            if (formDataCollection == null)
+            {
+                throw new ArgumentNullException(nameof(formDataCollection));
+            }
+
+            return ParseFromCollection(formDataCollection, typeof(T));
         }
 
-        public static object ParseFromCollection(this FormDataCollection formDataCollection, Type targetType)
+        public static object ParseFromCollection(
+            this FormDataCollection formDataCollection,
+            Type targetType)
         {
             if (formDataCollection == null)
             {
-                throw new ArgumentNullException("formDataCollection");
+                throw new ArgumentNullException(nameof(formDataCollection));
             }
 
             if (targetType == null)
             {
-                throw new ArgumentNullException("targetType");
+                throw new ArgumentNullException(nameof(targetType));
+            }
+
+            return ParseFromPairs(formDataCollection, targetType);
+        }
+
+        public static object ParseFromPairs(
+            IEnumerable<KeyValuePair<string, string>> collection,
+            Type targetType)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (targetType == null)
+            {
+                throw new ArgumentNullException(nameof(targetType));
             }
 
             var dynamicObject = new ExpandoObject();
 
-            var pairGroups = formDataCollection
+            var pairGroups = collection
                 .GroupBy(pair => pair.Key)
                 .Select(grouping => new
-                                    {
-                                        grouping.Key,
-                                        Values = grouping
-                                            .Select(item => item.Value)
-                                            .ToArray()
-                                    })
+                {
+                    grouping.Key,
+                    Values = grouping
+                        .Select(item => item.Value)
+                        .ToArray()
+                })
                 .ToArray();
+
+            var nested = pairGroups.Where(pair => pair.Key.Contains("[")).ToArray();
 
             IDictionary<string, object> dynamicObjectDictionary = dynamicObject;
 
-            var singleValuePairs = pairGroups.Where(pairGroup => pairGroup.Values.Count() == 1);
+            var singleValuePairs = pairGroups.Where(pairGroup => pairGroup.Values.Length == 1).Except(nested);
 
             foreach (var keyValuePair in singleValuePairs)
             {
                 dynamicObjectDictionary[keyValuePair.Key] = keyValuePair.Values.Single();
             }
 
-            var multipleValuesPairs = pairGroups.Where(pairGroup => pairGroup.Values.Count() >= 2);
+            var multipleValuesPairs = pairGroups.Where(pairGroup => pairGroup.Values.Length >= 2).Except(nested);
 
             foreach (var keyValuePair in multipleValuesPairs)
             {
@@ -63,12 +91,100 @@ namespace Arbor.WebApi.Formatting.HtmlForms
             {
                 object instance = JsonConvert.DeserializeObject(json, targetType);
 
+                dynamic asDynamic = instance;
+
+                if (instance != null)
+                {
+                    foreach (PropertyInfo propertyInfo in targetType.GetProperties()
+                        .Where(property => property.CanWrite &&
+                                           typeof(IEnumerable).IsAssignableFrom(
+                                               property.PropertyType) && property.PropertyType.IsGenericType))
+                    {
+                        Type subTargetType = propertyInfo.PropertyType.GenericTypeArguments.FirstOrDefault();
+
+                        string expectedName = propertyInfo.Name;
+
+                        var matchingProperty = nested.Select(nestedGroup =>
+                            {
+                                int indexIndex = nestedGroup.Key.IndexOf("[");
+                                int indexStopIndex = nestedGroup.Key.IndexOf("]");
+                                int indexLength = indexStopIndex - indexIndex;
+
+                                int dotIndex = nestedGroup.Key.IndexOf(".");
+
+                                string name = nestedGroup.Key.Substring(0, indexIndex);
+
+                                string index = nestedGroup.Key.Substring(indexIndex + 1, indexLength - 1);
+
+                                string propertyName = nestedGroup.Key.Substring(dotIndex + 1);
+
+                                return new { GroupName = name, nestedGroup.Values, Index = index, propertyName };
+                            })
+                            .Where(s => s.GroupName.Equals(expectedName, StringComparison.OrdinalIgnoreCase))
+                            .ToArray();
+
+                        var subPairs = new List<KeyValuePair<string, string>>();
+
+                        var indexedGroups = matchingProperty.GroupBy(_ => _.Index);
+
+                        foreach (var item in indexedGroups)
+                        {
+                            var pairs = new List<KeyValuePair<string, string>>();
+                            foreach (var value in item)
+                            {
+                                foreach (string valueProperty in value.Values)
+                                {
+                                    pairs.Add(new KeyValuePair<string, string>(value.propertyName, valueProperty));
+                                }
+                            }
+                            object subTargetInstance = ParseFromPairs(pairs, subTargetType);
+
+                            object currentCollection = propertyInfo.GetValue(instance);
+
+                            if (currentCollection == null)
+                            {
+                                object newCollection = null;
+
+                                if (!propertyInfo.PropertyType.IsAbstract)
+                                {
+                                    try
+                                    {
+                                        newCollection = Activator.CreateInstance(propertyInfo.PropertyType);
+                                    }
+                                    catch (Exception)
+                                    {
+                                        // Ignore exception
+                                    }
+                                }
+
+                                if (newCollection == null)
+                                {
+                                    throw new InvalidOperationException(
+                                        $"Could not create new {propertyInfo.PropertyType.FullName}");
+                                }
+
+                                propertyInfo.SetValue(instance, newCollection);
+
+                                currentCollection = newCollection;
+                            }
+
+                            var propertyCollection = currentCollection as IList;
+
+                            if (propertyCollection != null)
+                            {
+                                propertyCollection.Add(subTargetInstance);
+                            }
+                        }
+                    }
+                }
+
                 return instance;
             }
             catch (JsonSerializationException ex)
             {
                 throw new InvalidOperationException(
-                    string.Format("Could not deserialize type {0} from JSON '{1}'", targetType, json), ex);
+                    string.Format("Could not deserialize type {0} from JSON '{1}'", targetType, json),
+                    ex);
             }
         }
     }
